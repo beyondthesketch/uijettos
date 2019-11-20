d45be3af5949d3f91cbb2a8c3055f16a4179a543
Nazeerudeen Ruhomutally <NajuDev@users.noreply.github.com>
2019-11-20 18:34:29 +0000

    remove unnecessary code from index.js

--- index.js
+++ index.js
@@ -3,4 +3,3 @@ export {default as UijettosToggleButton} from './src/components/toggle-button/to
 export {default as UijettosSelectList} from './src/components/select-list/select-list';
 
 export {default as UijettosOptionsButton} from './src/components/options-button/options-button';
-export * from './src/components/options-button/options-button';
