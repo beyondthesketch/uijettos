720c3225a5b6296ec9a5776ded0b6f7af30996e5
Nazeerudeen Ruhomutally <NajuDev@users.noreply.github.com>
2020-01-11 13:03:22 +0000

    Issue #1 refactor to allow floating number use

--- src/components/spin-box/spin-box.js
+++ src/components/spin-box/spin-box.js
@@ -32,6 +32,7 @@ export default function UijettosSpinBox(
         '8',
         '9',
         '0',
+        '.',
         'Backspace',
         'ArrowUp',
         'ArrowDown',
@@ -40,29 +41,35 @@ export default function UijettosSpinBox(
     ];
     const [value, setValue] = useState( initialValue );
     const cssRootClass = `${cssClassPrefix}-spin-box`;
-    const reachedMax = !!(max && (max !== undefined) && ((value + 1) > max));
-    const reachedMin = !!(min && (min !== undefined) && ((value - 1) < min));
+    const newValue = parseFloat(value);
+    const reachedMax = !!(max && (max !== undefined) && (((newValue + 0.1) > max)) || ((newValue + 1) > max));
+    const reachedMin = !!(min && (min !== undefined) && (((newValue - 0.1) < min)) || ((newValue - 1) < min));
+    console.log(newValue, newValue - 0.1, newValue);
 
-    const increment = () => {
+    const increment = (event) => {
         if (reachedMax) {
             return null;
         }
-        const val = value + 1;
+        const shift = event.shiftKey;
+        const val = (newValue * 10 + (shift ? 0.1 : 1) * 10) / 10;
         (whenChanged && whenChanged(val));
         whenIncreased && whenIncreased(val);
-        return setValue(val);
+        return setValue(val.toString());
     };
-    const decrement = () => {
+    const decrement = (event) => {
         if (reachedMin) {
             return null;
         }
-        const val = value - 1;
+        const shift = event.shiftKey;
+        const val = (newValue * 10 - (shift ? 0.1 : 1) * 10) / 10;
         (whenChanged && whenChanged(val));
         whenDecreased && whenDecreased(val);
-        return setValue(val);
+        return setValue(val.toString());
     };
     const inputChange = (event) => {
-        let val = parseInt(event.target.value, 10) || undefined;
+        const stringValue = event.target.value;
+        let val = parseFloat(stringValue) || undefined;
+        console.log(stringValue.endsWith('.'), val);
 
         // TODO: Rethink UX around entering numbers directly that are no in min/max range
         if (min && (val < min)) {
@@ -73,10 +80,22 @@ export default function UijettosSpinBox(
         }
 
         return (
-            setValue(val),
+            setValue(
+                val === undefined
+                    ?
+                    ''
+                    :
+                    stringValue.endsWith('.') && val !== undefined
+                        ?
+                        stringValue
+                        :
+                        val.toString()
+            ),
             ((allowEmpty && whenChanged) || (val && whenChanged)) && whenChanged(val)
         );
     };
+
+    console.log(typeof value);
 
     return (
         <div
@@ -90,7 +109,7 @@ export default function UijettosSpinBox(
                 max={ max }
                 onKeyDown={
                     (event) => {
-                        if (!KEYS.includes(event.key)) {
+                        if (!KEYS.includes(event.key) || ((value && value.includes && value.includes('.')) && (event.key === '.'))) {
                             event.preventDefault();
                             return;
                         }
@@ -113,6 +132,16 @@ export default function UijettosSpinBox(
                             const val = min || 0;
                             setValue(val);
                             whenChanged && whenChanged(val);
+                        }
+                        else if (event.target.value.endsWith('.')) {
+                            const val = parseFloat(event.target.value);
+                            const newVal = max && (val < max)
+                                ?
+                                val
+                                :
+                                max || 0;
+                            setValue(newVal);
+                            whenChanged && whenChanged(newVal);
                         }
                     }
                 }
