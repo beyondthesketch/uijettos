450afda4ecd76fbcbc7b8064edcddc4ab01d4f24
Nazeerudeen Ruhomutally <NajuDev@users.noreply.github.com>
2020-04-17 10:03:49 +0100

    Issue #1: Spin box story updates and default styling changes

--- src/components/spin-box/spin-box.stories.js
+++ src/components/spin-box/spin-box.stories.js
@@ -5,22 +5,24 @@ import { action } from '@storybook/addon-actions';
 import { withCssResources } from '@storybook/addon-cssresources';
 import UijettosSpinBox from './spin-box';
 
+import defaultStyles from './../../sass/spin-box.defaults.scss';
+
 const stories = storiesOf('Spin Box', module);
 
 stories.addDecorator(withKnobs);
 
-// stories.addDecorator(withCssResources);
-// stories.addParameters(
-//     {
-//         cssresources: [
-//             {
-//                 id: `Spin Box Default`,
-//                 code: `<style></style>`,
-//                 picked: true
-//             },
-//         ]
-//     }
-// );
+stories.addDecorator(withCssResources);
+stories.addParameters(
+    {
+        cssresources: [
+            {
+                id: `Spin Box Default`,
+                code: `<style>${defaultStyles}</style>`,
+                picked: true
+            },
+        ]
+    }
+);
 
 stories
     .add(
@@ -30,7 +32,9 @@ stories
             const initialValue = number('initialValue', 3);
             const min = number('min', 2);
             const max = number('max', 5);
+            const step = number('step', undefined);
             const allowEmpty = boolean('allowEmpty', false);
+            const autoWidth = boolean('autoWidth', true);
 
             return (
                 <UijettosSpinBox
@@ -38,7 +42,9 @@ stories
                     initialValue={ initialValue }
                     min={ min }
                     max={ max }
+                    step={ step }
                     allowEmpty={ allowEmpty }
+                    autoWidth={ autoWidth }
                     whenChanged={ action('changed') }
                     whenDecreased={ action('decreased') }
                     whenIncreased={ action('increased') }
